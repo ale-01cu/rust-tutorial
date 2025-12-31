0e58ec9bfcf16fe63c91039dcc4ac4070b452be9
ale-01cu <alejandro.figueroa.rodriguez.01@gmail.com>
2025-12-30 21:00:26 -0500

    Fix: deprecated functions

--- 2_guessing_game/src/main.rs
+++ 2_guessing_game/src/main.rs
@@ -5,18 +5,18 @@ use rand::Rng;
 fn main() {
     println!("Guess the Number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number: u32 = rand::rng().random_range(1..=100);
 
     loop {
         println!("Please input your guess.");
         let mut guess = String::new();
-    
+
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
     
         println!("You guessed: {guess}");
-    
+
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue
