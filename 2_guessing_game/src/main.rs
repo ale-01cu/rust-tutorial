9729e1eccf45de2790f21bbe031abdc998a8bc9c
ale-01cu <alejandro.figueroa.rodriguez.01@gmail.com>
2025-12-31 09:46:12 -0500

    Variables

--- 2_guessing_game/src/main.rs
+++ 2_guessing_game/src/main.rs
@@ -21,7 +21,7 @@ fn main() {
             Ok(num) => num,
             Err(_) => continue
         };
-    
+
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
